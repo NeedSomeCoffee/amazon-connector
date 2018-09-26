aefc809be42fc65745fbdf5459adef84d565387a
Ivan <ivan.bai.sergeevich@gmail.com>
2018-09-26 22:01:20 +0300

    add feature: adding to cart

--- src/main/java/edu/amazon/models/pageobjects/ProductPage.java
+++ src/main/java/edu/amazon/models/pageobjects/ProductPage.java
@@ -1,12 +1,16 @@
 package edu.amazon.models.pageobjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 import edu.amazon.interfaces.ProductSearch;
 import edu.amazon.models.Product;
 import edu.amazon.util.ProductParser;
 
 public class ProductPage extends PageObject implements ProductSearch {
+	@FindBy(id = "add-to-cart-button-ubb")
+	private WebElement addToCartButton;
 	
 	public ProductPage(WebDriver driver) {
 		super(driver);
@@ -18,5 +22,11 @@ public class ProductPage extends PageObject implements ProductSearch {
 		parsed.setUrl(driver.getCurrentUrl());
 		
 		return parsed;
+	}
+	
+	public ProductPage addProductToCart() {
+		addToCartButton.click();
+		
+		return this;
 	}
 }
